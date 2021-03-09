15cdfd971afe1098e2eed64bd76a2746daa3259b
Jörg Strempfer <58751783+strempfer@users.noreply.github.com>
2021-03-08 22:11:45 -0600

    Update db_tools.py
    update show_meta and delete old show_meta

--- polartools/db_tools.py
+++ polartools/db_tools.py
@@ -12,93 +12,7 @@ from pyRestTable import Table
 from warnings import warn
 
 
-def show_meta(db, scan_from, scan_to=None, query=None, long=False):
-    """
-    Searches the databroker v2 database.
-    Parameters
-    ----------
-    db :
-        `databroker` database.
-    from: int
-        Scan number start
-    to: int
-        Scan number end
-    query: dict
-        Search parameters.
-    long: boolean
-        long output
-    Returns
-    -------
-    output :
-        List of scan metadata
-    """
-
-    db_range = db_query(db, query=query) if query else db
-    time_old = 0
-    if scan_to and scan_from < scan_to:
-        scan_to += 1
-    else:
-        scan_to = scan_from + 1
-    for scan_number in range(scan_from, scan_to):
-        try:
-            meta = db_range[scan_number].metadata["start"]
-
-        except KeyError:
-            print(f"Scan {scan_number} not existing!")
-            return
-
-        time_new = meta["time"]
-        time = datetime.fromtimestamp(time_new)
-        if time_new < time_old:
-            return
-        time_old = time_new
-        scan_type = meta["plan_name"] if "plan_name" in meta else None
-        motors = meta["motors"] if "motors" in meta else None
-
-        plan_args = (
-            meta["plan_pattern_args"] if "plan_pattern_args" in meta else None
-        )
-
-        if scan_type == "list_scan":
-            scan_from = plan_args["args"][1][0]
-            scan_to = plan_args["args"][1][-1]
-            scan_type2 = (
-                meta["hints"]["scan_type"]
-                if "scan_type" in meta["hints"]
-                else None
-            )
-        else:
-            scan_from = plan_args["args"][1]
-            scan_to = plan_args["args"][2]
-
-        number_of_points = meta["num_points"]
-        hint = meta["hints"]
-        det = hint["detectors"] if "detectors" in hint else None
-        mon = hint["monitor"] if "monitor" in hint else None
-        meta_stop = db[scan_number].metadata["stop"]
-        status = (
-            meta_stop["exit_status"]
-            if "exit_status" in meta_stop
-            else "running"
-        )
-
-        if scan_type == "list_scan":
-            print(
-                f"#{scan_number} {scan_type}[{scan_type2}] {motors} "
-                f"{scan_from} {scan_to} {number_of_points} {status}"
-            )
-        else:
-            print(
-                f"#{scan_number} {scan_type} {motors} {scan_from} {scan_to} "
-                f"{number_of_points} {status}"
-            )
-
-        if long:
-            print(f"        detector={det}, monitor={mon}")
-            print(f"        time: {time}")
-
-
-def show_meta_2(scans, db, scan_to=None, query=None, meta_keys="short"):
+def show_meta(scans, db, scan_to=None, query=None, meta_keys="short"):
     """
     Print metadata of scans.
 
@@ -162,19 +76,27 @@ def show_meta_2(scans, db, scan_to=None, query=None, meta_keys="short"):
         meta_keys.insert(index, "final pos.")
         meta_keys.insert(index, "init. pos.")
 
-    table.labels = ["Scan #"] + list(meta_keys)
-
+    table.labels = ["Scan # "] + list(meta_keys)
     for scanno, values in meta.items():
 
         row = [scanno]
+        lscan = False
+        cscan = False
         for key, item in values.items():
+            if item[0] == "list_scan" or item[0] == "slitscan":
+                lscan = True
+            if item[0] == "count":
+                cscan = True
             if key == "plan_pattern_args":
-                if item[0] is not None:
+                if lscan:
+                    row.append(item[0]["args"][1][0])
+                    row.append(item[0]["args"][1][-1])
+                elif cscan:
+                    row.append(None)
+                    row.append(None)
+                else:
                     row.append(item[0]["args"][-2])
                     row.append(item[0]["args"][-1])
-                else:
-                    row.append(None)
-                    row.append(None)
 
             elif key == "time":
                 time = []
@@ -192,7 +114,7 @@ def show_meta_2(scans, db, scan_to=None, query=None, meta_keys="short"):
                 row.append(item)
         table.rows.append(row)
 
-    print(table.reST(fmt="grid"))
+    print(table.reST(fmt="plain"))
 
 
 def collect_meta(scan_numbers, db, meta_keys, query=None):
