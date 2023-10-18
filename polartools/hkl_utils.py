d21d2306a45f323183a764bd2b413f3ae7169d52
Jörg Strempfer <58751783+strempfer@users.noreply.github.com>
2023-10-18 07:53:28 -0500

    Update hkl_utils.py
    file formatting

--- polartools/hkl_utils.py
+++ polartools/hkl_utils.py
@@ -261,7 +261,9 @@ def setor0(*args):
         if len(orienting_refl) > 1:
             for ref in sample._sample.reflections_get():
                 if ref == orienting_refl[0]:
-                    pos = ref.geometry_get().axis_values_get(_geom_.calc._units)
+                    pos = ref.geometry_get().axis_values_get(
+                        _geom_.calc._units
+                    )
                     old_delta = pos[4]
                     old_th = pos[1]
                     old_chi = pos[2]
@@ -365,7 +367,9 @@ def setor1(*args):
         if len(orienting_refl) > 1:
             for ref in sample._sample.reflections_get():
                 if ref == orienting_refl[1]:
-                    pos = ref.geometry_get().axis_values_get(_geom_.calc._units)
+                    pos = ref.geometry_get().axis_values_get(
+                        _geom_.calc._units
+                    )
                     old_delta = pos[4]
                     old_th = pos[1]
                     old_chi = pos[2]
@@ -896,6 +900,7 @@ def uan(delta=None, th=None):
         print("Moving to (delta,th)=({},{})".format(delta, th))
         yield from bps.mv(_geom_.delta, delta, _geom_.omega, th)
 
+
 def wh():
     """
     Retrieve information on the current reciprocal space position.
@@ -944,7 +949,9 @@ def setlat(a=None, b=None, c=None, alpha=None, beta=None, gamma=None):
 
     current_sample = _geom_.calc.sample_name
     sample = _geom_.calc._samples[current_sample]
-    lattice = [getattr(sample.lattice, parm) for parm in sample.lattice._fields]
+    lattice = [
+        getattr(sample.lattice, parm) for parm in sample.lattice._fields
+    ]
 
     a = input("Lattice a ({})? ".format(lattice[0])) if not a else a
     if not a:
@@ -956,15 +963,21 @@ def setlat(a=None, b=None, c=None, alpha=None, beta=None, gamma=None):
     if not c:
         c = lattice[2]
     alpha = (
-        input("Lattice alpha ({})? ".format(lattice[3])) if not alpha else alpha
+        input("Lattice alpha ({})? ".format(lattice[3]))
+        if not alpha
+        else alpha
     )
     if not alpha:
         alpha = lattice[3]
-    beta = input("Lattice beta ({})? ".format(lattice[4])) if not beta else beta
+    beta = (
+        input("Lattice beta ({})? ".format(lattice[4])) if not beta else beta
+    )
     if not beta:
         beta = lattice[4]
     gamma = (
-        input("Lattice gamma ({})? ".format(lattice[5])) if not gamma else gamma
+        input("Lattice gamma ({})? ".format(lattice[5]))
+        if not gamma
+        else gamma
     )
     if not gamma:
         gamma = lattice[5]
