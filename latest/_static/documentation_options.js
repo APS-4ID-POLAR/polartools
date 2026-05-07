49bec22b8fd26a20aaaf7ff08e2d2016e255d0e9
gfabbris <gfabbris@users.noreply.github.com>
2026-05-07 22:46:17 +0000

    deploy: 91d2811ec8eeecf054a2047eb554899c0379287c

---
+++ latest/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.5.5.post54+g91d2811ec',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
