f0674d9a64e4ad1c1d0952665bbdf54ca907ce44
gfabbris <gfabbris@users.noreply.github.com>
2026-05-08 20:44:48 +0000

    deploy: bd92da912472e8df083a85f4735332eb2b3ab708

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.5.post56+g8e9f06207',
+    VERSION: '0.6.0.post1+gbd92da912',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
